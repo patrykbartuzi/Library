8a5abce5061343523d04450f85500b4f889762e4
patryk.bartuzi <patryk.bartuzii@gmail.com>
2024-10-28 19:33:58 +0100

    static final variables added

--- src/main/java/pl/javastart/library/app/LibraryApp.java
+++ src/main/java/pl/javastart/library/app/LibraryApp.java
@@ -1,9 +1,9 @@
 package pl.javastart.library.app;
 
 public class LibraryApp {
+        final static  String APP_NAME = "Biblioteka v1.0";
     public static void main(String[] args) {
-        final String appName = "Biblioteka v0.9";
-        System.out.println(appName);
+        System.out.println(APP_NAME);
 
         LibraryControl libControl = new LibraryControl();
         libControl.controlLoop();
