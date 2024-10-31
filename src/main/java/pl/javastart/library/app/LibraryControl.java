9c1a25b1078ff7a11c307748b64440f0285d74d5
patryk.bartuzi <patryk.bartuzii@gmail.com>
2024-10-31 15:09:12 +0100

    inheritance applied and magazines added

--- src/main/java/pl/javastart/library/app/LibraryControl.java
+++ src/main/java/pl/javastart/library/app/LibraryControl.java
@@ -3,13 +3,18 @@ package pl.javastart.library.app;
 import pl.javastart.library.io.DataReader;
 import pl.javastart.library.model.Book;
 import pl.javastart.library.model.Library;
+import pl.javastart.library.model.Magazine;
 
 public class LibraryControl {
-    private final static int EXIT = 0;
-    private final static int ADD_BOOK = 1;
-    private final static int PRINT_BOOKS = 2;
+    // stałe do kontrolowania programu
+    private static final int EXIT = 0;
+    private static final int ADD_BOOK = 1;
+    private static final int ADD_MAGAZINE = 2;
+    private static final int PRINT_BOOKS = 3;
+    private static final int PRINT_MAGAZINES = 4;
 
     private DataReader dataReader = new DataReader();
+
     private Library library = new Library();
 
     public void controlLoop() {
@@ -22,8 +27,14 @@ public class LibraryControl {
                 case ADD_BOOK:
                     addBook();
                     break;
+                case ADD_MAGAZINE:
+                    addMagazine();
+                    break;
                 case PRINT_BOOKS:
                     printBooks();
+                    break;
+                case PRINT_MAGAZINES:
+                    printMagazines();
                     break;
                 case EXIT:
                     exit();
@@ -31,14 +42,16 @@ public class LibraryControl {
                 default:
                     System.out.println("Nie ma takiej opcji, wprowadź ponownie: ");
             }
-        } while(option != EXIT);
+        } while (option != EXIT);
     }
 
     private void printOptions() {
         System.out.println("Wybierz opcję: ");
         System.out.println(EXIT + " - wyjście z programu");
         System.out.println(ADD_BOOK + " - dodanie nowej książki");
+        System.out.println(ADD_MAGAZINE + " - dodanie nowego magazynu");
         System.out.println(PRINT_BOOKS + " - wyświetl dostępne książki");
+        System.out.println(PRINT_MAGAZINES + " - wyświetl dostępne magazyny");
     }
 
     private void addBook() {
@@ -48,6 +61,15 @@ public class LibraryControl {
 
     private void printBooks() {
         library.printBooks();
+    }
+
+    private void addMagazine() {
+        Magazine magazine = dataReader.readAndCreateMagazine();
+        library.addMagazine(magazine);
+    }
+
+    private void printMagazines() {
+        library.printMagazines();
     }
 
     private void exit() {
