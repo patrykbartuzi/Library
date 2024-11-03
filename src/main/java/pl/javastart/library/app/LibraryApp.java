38b00e70a7a744d4bb8be7717a0d1c8f070878de
patryk.bartuzi <patryk.bartuzii@gmail.com>
2024-11-03 18:48:20 +0100

    enum() Option added

--- src/main/java/pl/javastart/library/app/LibraryApp.java
+++ src/main/java/pl/javastart/library/app/LibraryApp.java
@@ -1,7 +1,7 @@
 package pl.javastart.library.app;
 
 public class LibraryApp {
-        final static  String APP_NAME = "Biblioteka v1.1";
+        final static  String APP_NAME = "Biblioteka v1.2";
     public static void main(String[] args) {
         System.out.println(APP_NAME);
 
