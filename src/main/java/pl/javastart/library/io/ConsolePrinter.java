c6dcb3264c705adfa35c774cb40789df61a94612
patryk.bartuzi <patryk.bartuzii@gmail.com>
2025-01-13 21:21:26 +0100

    stream2 added

--- src/main/java/pl/javastart/library/io/ConsolePrinter.java
+++ src/main/java/pl/javastart/library/io/ConsolePrinter.java
@@ -9,27 +9,23 @@ import java.util.Collection;
 
 public class ConsolePrinter {
     public void printBooks(Collection<Publication> publications) {
-        int counter = 0;
-        for (Publication publication : publications) {
-            if (publication instanceof Book) {
-                printLine(publication.toString());
-                counter++;
-            }
-            if (counter == 0)
-                printLine("Brak książek w bibliotece");
-        }
+        long counter = publications
+                .stream()
+                .filter(p -> p instanceof Book)
+                .peek(System.out::println)
+                .count();
+        if (counter == 0)
+            printLine("Brak książek w bibliotece");
     }
 
     public void printMagazines(Collection<Publication> publications) {
-        int counter = 0;
-        for (Publication publication : publications) {
-            if (publication instanceof Magazine) {
-                printLine(publication.toString());
-                counter++;
-            }
-            if (counter == 0)
-                printLine("Brak magazynów w bibliotece");
-        }
+        long counter = publications
+                .stream()
+                .filter(p -> p instanceof Magazine)
+                .peek(System.out::println)
+                .count();
+        if (counter == 0)
+            printLine("Brak magazynów w bibliotece");
     }
 
     public void printUsers(Collection<LibraryUser> users) {
@@ -37,6 +33,7 @@ public class ConsolePrinter {
             printLine(user.toString());
         }
     }
+
 
     public void printLine(String text) {
         System.out.println(text);
